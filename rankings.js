4f964415d18e3a688d990f14f7fe56b0e962b2be
LMSC4L <99811015+LMSC4L@users.noreply.github.com>
2025-01-03 10:07:43 -0500

    Update and rename Rankings.js to rankings.js

---
+++ rankings.js
@@ -0,0 +1,35 @@
+// Rank Number 1
+// The Player Number For The Ranked Player (System Will Automatically Use Their Name, It's Hard Coded.)
+let rank1playernum = 6;
+let rank1wins = 14;
+let rank1loss = 6;
+
+// Rank Number 2
+// The Player Number For The Ranked Player (System Will Automatically Use Their Name, It's Hard Coded.)
+let rank2playernum = 7;
+let rank2wins = 13;
+let rank2loss = 7;
+
+// Rank Number 3
+// The Player Number For The Ranked Player (System Will Automatically Use Their Name, It's Hard Coded.)
+let rank3playernum = 3;
+let rank3wins = 12;
+let rank3loss = 8;
+
+// Rank Number 4
+// The Player Number For The Ranked Player (System Will Automatically Use Their Name, It's Hard Coded.)
+let rank4playernum = 4;
+let rank4wins = 11;
+let rank4loss = 9;
+
+// Rank Number 5
+// The Player Number For The Ranked Player (System Will Automatically Use Their Name, It's Hard Coded.)
+let rank5playernum = 7;
+let rank5wins = 10;
+let rank5loss = 10;
+
+// Rank Number 6
+// The Player Number For The Ranked Player (System Will Automatically Use Their Name, It's Hard Coded.)
+let rank6playernum = 2;
+let rank6wins = 9;
+let rank6loss = 11;
