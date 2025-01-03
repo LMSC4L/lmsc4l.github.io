f84998f7795396876276d5606bcd70eb29947784
LMSC4L <99811015+LMSC4L@users.noreply.github.com>
2025-01-03 11:33:53 -0500

    Update rankings.js

--- rankings.js
+++ rankings.js
@@ -1,24 +1,33 @@
 // In p#wins and p#loss, # is the player number.
 let p1wins = 8;
 let p1loss = 12;
+let p1rank = 7;
 
 let p2wins = 9;
 let p2loss = 11;
+let p2rank = 6;
 
 let p3wins = 12;
 let p3loss = 8;
+let p3rank = 3;
 
 let p4wins = 11;
 let p4loss = 9;
+let p4rank = 4;
 
 let p5wins = 7;
 let p5loss = 13;
+let p5rank = 8;
 
 let p6wins = 14;
 let p6loss = 6;
+let p6rank = 1;
 
 let p7wins = 10;
 let p7loss = 10;
+let p7rank = 5;
 
 let p8wins = 13;
 let p8loss = 7;
+let p8rank = 2;
+
